5ed10b073110a3facf694bbc700fbee580d25d3c
KainyGuo <gt@kainy.cn>
2019-02-25 20:04:13 +0800

    code

--- config.js
+++ config.js
@@ -3,23 +3,23 @@ module.exports = {
 	title: 'Zing Gallery',
 	avatar: './img/avatar.png',
 	wording: {
-		noAccess: 'æŠ±æ­‰ï¼Œä½ æ²¡æœ‰æƒé™è®¿é—®'
+		noAccess: '±§Ç¸£¬ÄãÃ»ÓĞÈ¨ÏŞ·ÃÎÊ'
 	},
 	albums: {
-		"é’æµ·æ¹–": {
-			thumbnail: "æ‰“é©¬è€Œè¿‡.jpg",
+		"Çàº£ºş": {
+			thumbnail: "´òÂí¶ø¹ı.jpg",
 		  sort: 1
 		}, 
-		"æ±Ÿå—å°é•‡": {
-		  description : "æ²¡æœ‰ä»€ä¹ˆèƒ½å¤Ÿé˜»æŒ¡",
-			thumbnail: "ç‡•æ­Œè¡Œ.jpg",
-		  name: "å—æµ”"
+		"½­ÄÏĞ¡Õò": {
+		  description : "Ã»ÓĞÊ²Ã´ÄÜ¹»×èµ²",
+			thumbnail: "Ñà¸èĞĞ.jpg",
+		  name: "ÄÏä±"
 		},
-		"ç§å¯†": {
-			description: "è¿™æ˜¯ä¸€ä¸ªéœ€è¦å¯†ç çš„ç›¸å†Œ",
-		  name: "ç§å¯†",
+		"Ë½ÃÜ": {
+			description: "ÕâÊÇÒ»¸öĞèÒªÃÜÂëµÄÏà²á",
+		  name: "Ë½ÃÜ",
 		  password: "233",
-			passwordTips: "å¯†ç æ˜¯233"
+			passwordTips: "ÃÜÂëÊÇ233"
 		}
 	}
 }
