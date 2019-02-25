ffd8f95ea876ef75d3ddf6b316a1497d8643277a
KainyGuo <gt@kainy.cn>
2019-02-25 18:21:17 +0800

    modi

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
+var AV = require('leanengine');
+
+AV.init({
+  appId: process.env.LEANCLOUD_APP_ID,
+  appKey: process.env.LEANCLOUD_APP_KEY,
+  masterKey: process.env.LEANCLOUD_APP_MASTER_KEY
+});
+
+// 如果不希望使用 masterKey 权限，可以将下面一行删除
+AV.Cloud.useMasterKey();
+
+
+
 require('events').EventEmitter.prototype._maxListeners = 100;
 var resize = require('./lib/resize'),
 cfg = require('./config'),
 express = require('express'),
-port = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3000,
 host = process.env.OPENSHIFT_NODEJS_IP;
 
 var photosPath = './resources/photos';
@@ -23,7 +35,10 @@ app.use('/', require('./lib/gallery.js')(Object.assign({
   }, cfg));
 });
 
+// 端口一定要从环境变量 `LEANCLOUD_APP_PORT` 中获取。
+// LeanEngine 运行时会分配端口并赋值到该变量。
+var PORT = parseInt(process.env.LEANCLOUD_APP_PORT || process.env.PORT || 3000);
 
-app.listen(port, host);
+app.listen(PORT, host);
 host = host || 'localhost';
-console.log('zing-gallery listening on ' + host  + ':' + port);
+console.log('zing-gallery listening on ' + host  + ':' + PORT);
