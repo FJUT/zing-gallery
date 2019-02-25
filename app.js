ba689e919ed8181b7a07b5a8cf81a70d39eef040
KainyGuo <gt@kainy.cn>
2019-02-25 19:54:44 +0800

    取消图片优化，leancoud不支持。

--- app.js
+++ app.js
@@ -7,9 +7,10 @@ AV.init({
 });
 
 // 如果不希望使用 masterKey 权限，可以将下面一行删除
-AV.Cloud.useMasterKey();
+// AV.Cloud.useMasterKey();
 
-
+// 加载云函数定义，你可以将云函数拆分到多个文件方便管理，但需要在主文件中加载它们
+require('./cloud');
 
 require('events').EventEmitter.prototype._maxListeners = 100;
 var resize = require('./lib/resize'),
@@ -24,6 +25,15 @@ var app = express();
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/assets/dist/'));
+app.get('/1.1/functions/_ops/metadatas', function(req, res, next) {
+  // 如果任何一个路由都没有返回响应，则抛出一个 404 异常给后续的异常处理器
+  if (!res.headersSent) {
+    res.status(404).send('Sorry cant find that!');
+    // var err = new Error('Not Found');
+    // err.status = 404;
+    // next(err);
+  }
+});
 app.use('/', require('./lib/gallery.js')(Object.assign({
   staticFiles : 'resources/photos',
   urlRoot : '/',
